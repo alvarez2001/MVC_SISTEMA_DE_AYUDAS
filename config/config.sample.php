94e2dc064573112026074ca2a06b1e1ba243173d
alvarez2001 <josemiguelalvarez2014@gmail.com>
2020-11-28 12:41:29 -0400

    add config.sample.php

---
+++ config/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+define('URL', 'http://localhost/mvc');
+
+define('HOST', 'localhost');
+define('DB', 'mvc_app');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
